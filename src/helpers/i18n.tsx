ab0f1bc2657a38819a7814a9435763bfd26155db
zbahadir <zbahadir@msn.com>
2020-06-03 13:35:31 +0300

    Updated

--- src/helpers/i18n.tsx
+++ src/helpers/i18n.tsx
@@ -2,16 +2,17 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-xhr-backend';
-
+/*
 const defaultLng = 'tr';
 let lng = defaultLng;
+*/
 
 i18n
   .use(LanguageDetector)
   .use(Backend)
   .use(initReactI18next)
   .init({
-    lng,
+    //lng,
     debug: true,
     fallbackLng: 'en',    
     backend: {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonIcon, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
 import React from 'react';
 import './Home.css';
-// import {Translation} from 'react-i18next';
-
-// import i18n from 'react-i18next';
+import { languageOutline } from 'ionicons/icons';
 import {useTranslation} from 'react-i18next';
-// import i18next, { i18n } from 'i18next';
-
+import i18n from '../helpers/i18n';
 
 const Home: React.FC = () => {
 
 const {t} = useTranslation("home");
 
 
-
-  // i18next.changeLanguage('en-EN');
-  //const {t} = useTranslation();
-
-  
-  /*
-  const allowedLanguages = ['en', 'tr'];
-
-  const defaultLng = 'en';
-  let lng = defaultLng;
-*/
-/*
-{t('search.clients')}
-*/
   return (
-    
+     
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>{t('zafer')}</IonTitle>
+          <IonTitle>{t('main.title')}</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large"></IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonIcon slot="start" icon={languageOutline}></IonIcon>
+          <IonLabel position="stacked">{t('selector_title')}</IonLabel>
+          <IonSelect value={i18n.language} okText={t('ok')} cancelText={t('dismiss')} onIonChange={e => i18n.changeLanguage(e.detail.value)}>
+          <IonSelectOption value="en">{t('english')}</IonSelectOption>
+          <IonSelectOption value="tr">{t('turkish')}</IonSelectOption>
+          <IonSelectOption value="fr">{t('french')}</IonSelectOption>
+          </IonSelect>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
